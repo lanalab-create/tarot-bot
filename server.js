b07e00de252e4a57e760215a3960722e28f3d5e5
lanalab-create <allanaleticiapontes.afiliada@gmail.com>
2025-04-30 19:31:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -123,7 +123,6 @@ app.post('/tarot-reading', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+app.listen(3000, () => {
+  console.log("Tarot bot is running on port 3000");
 });
